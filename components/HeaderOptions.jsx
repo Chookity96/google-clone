877e5fe6f7f1c11309d59880c0dee790a5dcf63f
Chookity96 <jegathees35@gmail.com>
2022-07-01 03:10:33 +0800

    bug fix

--- components/HeaderOptions.jsx
+++ components/HeaderOptions.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { DotsVerticalIcon, MapIcon, NewspaperIcon, PhotographIcon, PlayIcon, SearchIcon, } from '@heroicons/react/outline'
+import { DotsVerticalIcon, MapIcon, NewspaperIcon, PhotographIcon, PlayIcon, SearchIcon,  } from '@heroicons/react/outline'
 import HeaderOption from './HeaderOption'
 
 function HeaderOptions() {
 	return (
-		<div className="flex w-full pl-6 pr-8 text-gray-700 justify-between text-sm lg:text-base border-b sm:pr-16 sm:pl-[5%] md:pl-[10%] lg:pr-52 xl:pr-[430px] dark:text-white dark:border-b-slate-500">
+		<div className="flex w-full pl-6 text-gray-700 justify-between text-sm lg:text-base border-b sm:pl-[5%] md:pl-[10%] dark:text-white dark:border-b-slate-500">
 			<div className="flex space-x-6">
 				<HeaderOption title="All" Icon={SearchIcon} selected/>
 				<HeaderOption title="Images" Icon={PhotographIcon} />
@@ -12,10 +12,11 @@ function HeaderOptions() {
 				<HeaderOption title="News" Icon={NewspaperIcon} />
 				<HeaderOption title="Maps" Icon={MapIcon} />
 				<HeaderOption title="More" Icon={DotsVerticalIcon} />
+				<p className='link ' >Tools</p>
 			</div>
 
 			<div className=''>
-				<p className='link' >Tools</p>
+				{/* <p className='link' >Tools</p> */}
 			</div>
 		</div>
 	)
